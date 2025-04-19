3f816c58beb4b5aa8a428763989c407f182036d5
Abhijit Chakraborty <50722159+Abhijit85@users.noreply.github.com>
2025-04-19 02:23:27 -0700

    Update visura.py

--- agents/visura.py
+++ agents/visura.py
@@ -1,1 +1,13 @@
 # Visura agent implementation
+from bs4 import BeautifulSoup
+
+html_table = '''
+<table>
+  <tr><th><b>Product</b></th><th><span style="color:red">Profit</span></th></tr>
+  <tr><td>A</td><td>$100K</td></tr>
+</table>
+'''
+
+soup = BeautifulSoup(html_table, 'html.parser')
+emphasis = [(tag.name, tag.text) for tag in soup.find_all(['b', 'span'])]
+print("Visual Cues:", emphasis)
