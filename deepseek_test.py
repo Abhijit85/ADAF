a41feaf4484676522c7c9253dd32fb70d819f58e
shekharashishraj <shekharashishraj@gmail.com>
2025-07-22 18:03:06 -0700

    key recovery

--- deepseek_test.py
+++ deepseek_test.py
@@ -2,7 +2,7 @@ from openai import OpenAI
 
 client = OpenAI(
   base_url="https://openrouter.ai/api/v1",
-  api_key="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",
+  api_key="",
 )
 
 completion = client.chat.completions.create(
